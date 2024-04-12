78a58fe6fa18f37417b3f0fd46ca8bd7451eb784
Terry Holmes <tchaser3@outlook.com>
2024-04-12 07:11:24 -0400

    Add project files.

---
+++ ExpanderPractice/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpanderPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void expFirstExpander_Expanded(object sender, RoutedEventArgs e)
+        {
+            expSecondExpander.IsExpanded = false;
+        }
+
+        private void expSecondExpander_Expanded(object sender, RoutedEventArgs e)
+        {
+            expFirstExpander.IsExpanded = false;
+        }
+    }
+}
